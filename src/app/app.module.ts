5fb1ddb101bacc5145324987baf5bec0afb87eee
Atuar Rahman <atuar.cse@gmail.com>
2020-08-24 14:49:32 +0600

    Initiated client crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CreateEmailDomainComponent } from './component/emaildomain/create-email-domain/create-email-domain.component';
-import { EmailDomainListComponent } from './component/emaildomain/email-domain-list/email-domain-list.component';
-import { EmailDomainDetailsComponent } from './component/emaildomain/email-domain-details/email-domain-details.component';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import {FormsModule} from '@angular/forms';
+import { CreateEmailDomainComponent } from './component/emaildomain/create-email-domain/create-email-domain.component';
+import { EmailDomainListComponent } from './component/emaildomain/email-domain-list/email-domain-list.component';
+import { EmailDomainDetailsComponent } from './component/emaildomain/email-domain-details/email-domain-details.component';
 import { UpdateEmailDomainComponent } from './component/emaildomain/update-email-domain/update-email-domain.component';
+import { CreateClientComponent } from './component/client/create-client/create-client.component';
+import { ClientListComponent } from './component/client/client-list/client-list.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { UpdateEmailDomainComponent } from './component/emaildomain/update-email
     CreateEmailDomainComponent,
     EmailDomainListComponent,
     EmailDomainDetailsComponent,
-    UpdateEmailDomainComponent
+    UpdateEmailDomainComponent,
+    CreateClientComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/model/client.ts
@@ -0,0 +1,11 @@
+export class Client {
+  id: number;
+  customerName: string;
+  companyName: string;
+  profession: string;
+  address: string;
+  areaName: string;
+  divisionName: string;
+  contactNo: string;
+  emailAddress: string;
+}
